79d85167456d4bd5ecf67b6d1dbffcc43ab88ac3
Yakau Trafimovich <5243212@gmail.com>
2022-03-24 19:50:34 +0100

    add consructor

--- stack.js
+++ stack.js
@@ -1,5 +1,17 @@
+const stackMaxSize = 10;
+
 class Stack {
-    
+    constructor(itemsLimit = stackMaxSize) {
+        const isLimitIncorrect = !Number.isInteger(itemsLimit) || itemsLimit <= 0 || itemsLimit > stackMaxSize;
+        
+        if(isLimitIncorrect) {
+            throw new Error(`Лимит должен быть числом от 1 до ${stackMaxSize}, ${itemsLimit}`);
+        }
+
+        this.stack = [];
+        this.top = 0;
+        this.ignoreLimit = false;
+    }    
 }
 
-module.exports = { Stack }
+module.exports = { Stack };
