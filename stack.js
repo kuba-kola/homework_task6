f736140653e26ae55db6b56548ae80bc3f99fbaa
Yakau Trafimovich <5243212@gmail.com>
2022-03-25 07:50:46 +0100

    add Entry class

--- stack.js
+++ stack.js
@@ -1,20 +1,31 @@
-const stackMaxSize = 10;
+const stackMaxSize = 10
+
+class Entry {
+    constructor(value, prev, index) {
+        this.value = value
+        this.prev = prev
+        this.index = index
+    }
+
+    toArray() {
+        return [this.value, ...(this.prev ? this.prev.toArray() : [])]
+    }
+}
 
 class Stack {
     constructor(itemsLimit = stackMaxSize) {
-        const isLimitIncorrect = !Number.isInteger(itemsLimit) || itemsLimit <= 0 || itemsLimit > stackMaxSize;
-        
+        const isLimitIncorrect = !Number.isInteger(itemsLimit) || itemsLimit <= 0 || itemsLimit > stackMaxSize
+
         if(isLimitIncorrect) {
             throw new Error(`Лимит должен быть числом от 1 до ${stackMaxSize}, ${itemsLimit}`);
         }
 
-        this.stack = [];
-        this.top = 0;
-        this.ignoreLimit = false;
+        this.topEntry = null
+        this.ignoreLimit = false
     }
 
     push(item) {
-        if(!this.ignoreLimit && this.stack.length === stackMaxSize) {
+        if(!this.ignoreLimit && this.topEntry?.index === stackMaxSize) {
             throw new Error(`Стэк переполнен`);
         }
 
