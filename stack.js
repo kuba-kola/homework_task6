e6601faf6872712abde1b05ebe44c86c4366097b
Yakau Trafimovich <5243212@gmail.com>
2022-03-25 07:52:20 +0100

    change push method

--- stack.js
+++ stack.js
@@ -1,27 +1,27 @@
-const stackMaxSize = 10
+const stackMaxSize = 10;
 
 class Entry {
     constructor(value, prev, index) {
-        this.value = value
-        this.prev = prev
-        this.index = index
+        this.value = value;
+        this.prev = prev;
+        this.index = index;
     }
 
     toArray() {
-        return [this.value, ...(this.prev ? this.prev.toArray() : [])]
+        return [this.value, ...(this.prev ? this.prev.toArray() : [])];
     }
 }
 
 class Stack {
     constructor(itemsLimit = stackMaxSize) {
-        const isLimitIncorrect = !Number.isInteger(itemsLimit) || itemsLimit <= 0 || itemsLimit > stackMaxSize
+        const isLimitIncorrect = !Number.isInteger(itemsLimit) || itemsLimit <= 0 || itemsLimit > stackMaxSize;
 
         if(isLimitIncorrect) {
             throw new Error(`Лимит должен быть числом от 1 до ${stackMaxSize}, ${itemsLimit}`);
         }
 
-        this.topEntry = null
-        this.ignoreLimit = false
+        this.topEntry = null;
+        this.ignoreLimit = false;
     }
 
     push(item) {
@@ -29,8 +29,9 @@ class Stack {
             throw new Error(`Стэк переполнен`);
         }
 
-        this.stack = [...this.stack, item];
-        this.top++;
+        const index = this.topEntry ? (this.topEntry.index + 1) : 0;
+
+        this.topEntry = new Entry(item, this.topEntry, index);
     }
 
     pop() {
